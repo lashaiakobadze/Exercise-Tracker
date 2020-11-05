37ab83d6d5f4331b22e6c1bfb8fbdf4f157331d3
lashaiakobadze <70689627+lashaiakobadze@users.noreply.github.com>
2020-11-05 19:08:24 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
-const cors = require('cors')
-
-const mongoose = require('mongoose')
-let uri='mongodb+srv://Lasha_Iakobadze:'+process.env.PW+'@freecodecamp.ytjr4.mongodb.net/db1?retryWrites=true&w=majority';
+const cors = require("cors");
+const PW=lasha123321;
+const mongoose = require("mongoose");
+let uri =
+  "mongodb+srv://Lasha_Iakobadze:" +
+  process.env.PW +
+  "@freecodecamp.ytjr4.mongodb.net/db1?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
 /*
 let exerciseSessionSchema=new mongoose.Schema({
   description:{type:String,required:true},
@@ -42,107 +44,112 @@ app.post('/api/exercise/new-user',bodyParser.urlencoded({ extended: false }),(re
 })
 */
 let exerciseSchema = new mongoose.Schema({
-  description: {type: String, required: true},
-  duration: {type: Number, required: true},
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
   date: String
-})
+});
 
 let userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
+  username: { type: String, required: true },
   log: [exerciseSchema]
-})
+});
 
-let User = mongoose.model('User', userSchema)
-let Exercise = mongoose.model('Exercise', exerciseSchema)
+let User = mongoose.model("User", userSchema);
+let Exercise = mongoose.model("Exercise", exerciseSchema);
 
 /* Creating Users */
-app.post('/api/exercise/new-user', bodyParser.urlencoded({extended: false}),(request, response) => {
-  let newUser = new User({username: request.body.username})
-  newUser.save((error, savedUser) => {
-    if(!error){
-      response.json({username: savedUser.username, _id: savedUser.id})
-    }
-  })
-})
+app.post(
+  "/api/exercise/new-user",
+  bodyParser.urlencoded({ extended: false }),
+  (request, response) => {
+    let newUser = new User({ username: request.body.username });
+    newUser.save((error, savedUser) => {
+      if (!error) {
+        response.json({ username: savedUser.username, _id: savedUser.id });
+      }
+    });
+  }
+);
 
 /* Get all Users */
-app.get('/api/exercise/users', (request, response) => {
+app.get("/api/exercise/users", (request, response) => {
   User.find({}, (error, arrayOfUsers) => {
-    if(!error){
-      response.json(arrayOfUsers)
+    if (!error) {
+      response.json(arrayOfUsers);
     }
-  })
-})
+  });
+});
 
-app.post('/api/exercise/add', bodyParser.urlencoded({extended: false}), (request, response) => {
-  
-  let newExerciseItem = new Exercise({
-    description: request.body.description,
-    duration: parseInt(request.body.duration),
-    date: request.body.date
-  })
-  
-  if(newExerciseItem.date === ''){
-    newExerciseItem.date = new Date().toISOString().substring(0,10)
-  }
-  
-  User.findByIdAndUpdate(
-    request.body.userId,
-    {$push: {log: newExerciseItem}},
-    {new: true},
-    (error, updatedUser) => {
-    if(!error){
-      let responseObject = {}
-      responseObject['_id'] = updatedUser.id
-      responseObject['username']=updatedUser.username,
-      responseObject['date']=new Date(newExerciseItem.date).toDateString()
-      responseObject['description']=newExerciseItem.description
-      responseObject['duration']=newExerciseItem.duration
-      response.json(responseObject)
+app.post(
+  "/api/exercise/add",
+  bodyParser.urlencoded({ extended: false }),
+  (request, response) => {
+    let newExerciseItem = new Exercise({
+      description: request.body.description,
+      duration: parseInt(request.body.duration),
+      date: request.body.date
+    });
+
+    if (newExerciseItem.date === "") {
+      newExerciseItem.date = new Date().toISOString().substring(0, 10);
     }
-  })
-})
+
+    User.findByIdAndUpdate(
+      request.body.userId,
+      { $push: { log: newExerciseItem } },
+      { new: true },
+      (error, updatedUser) => {
+        if (!error) {
+          let responseObject = {};
+          responseObject["_id"] = updatedUser.id;
+          (responseObject["username"] = updatedUser.username),
+            (responseObject["date"] = new Date(
+              newExerciseItem.date
+            ).toDateString());
+          responseObject["description"] = newExerciseItem.description;
+          responseObject["duration"] = newExerciseItem.duration;
+          response.json(responseObject);
+        }
+      }
+    );
+  }
+);
 
 /* Retrieve a User's Log */
-app.get('/api/exercise/log', (request, response) => {
-  
+app.get("/api/exercise/log", (request, response) => {
   User.findById(request.query.userId, (error, result) => {
-    if(!error){
-      let responseObject = result
-      
-      if(request.query.from || request.query.to){
-        
-        let fromDate = new Date(0)
-        let toDate = new Date()
-        
-        if(request.query.from){
-          fromDate = new Date(request.query.from)
+    if (!error) {
+      let responseObject = result;
+
+      if (request.query.from || request.query.to) {
+        let fromDate = new Date(0);
+        let toDate = new Date();
+
+        if (request.query.from) {
+          fromDate = new Date(request.query.from);
         }
-        
-        if(request.query.to){
-          toDate = new Date(request.query.to)
+
+        if (request.query.to) {
+          toDate = new Date(request.query.to);
         }
-        
-        fromDate = fromDate.getTime()
-        toDate = toDate.getTime()
-        
-        responseObject.log = responseObject.log.filter((session) => {
-          let sessionDate = new Date(session.date).getTime()
-          
-          return sessionDate >= fromDate && sessionDate <= toDate
-          
-        })
-        
+
+        fromDate = fromDate.getTime();
+        toDate = toDate.getTime();
+
+        responseObject.log = responseObject.log.filter(session => {
+          let sessionDate = new Date(session.date).getTime();
+
+          return sessionDate >= fromDate && sessionDate <= toDate;
+        });
       }
-      
-      if(request.query.limit){
-        responseObject.log = responseObject.log.slice(0, request.query.limit)
+
+      if (request.query.limit) {
+        responseObject.log = responseObject.log.slice(0, request.query.limit);
       }
-      
-      responseObject = responseObject.toJSON()
-      responseObject['count'] = result.log.length
-      response.json(responseObject)
+
+      responseObject = responseObject.toJSON();
+      responseObject["count"] = result.log.length;
+      response.json(responseObject);
     }
-  })
-  
-})
+  });
+});
